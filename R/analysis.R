b3328182a687ab2964098b4b9702953dad00c2e6
Jiayu Li <jiayuliaq@gmail.com>
2023-07-01 23:14:42 +0800

    add plots of raw data

--- R/analysis.R
+++ R/analysis.R
@@ -1,17 +1,14 @@
 source("R/read_clean_data.R")
 
 dt_innova[!is.na(test_id)] %>% 
-  ggplot(aes(datetime, sf6_conc, color = paste("Ch", innova_ch))) +
+  ggplot(aes(datetime, sf6_conc, color = paste(loc, s_r, "\nCh.", innova_ch))) +
   geom_line() + 
   facet_wrap(vars(test_id), scales = "free") +
-  scale_color_discrete("Innova Ch")
+  scale_color_discrete("locations")+
+  guides(color = guide_legend(nrow = 1)) +
+  theme(legend.position = "top")
 
-dt_innova[!is.na(test_id)] %>% 
-  ggplot(aes(datetime, sf6_conc, color = paste(loc, s_r))) +
-  geom_line() + 
-  facet_wrap(vars(test_id), scales = "free") +
-  scale_color_discrete("locations")
-
+ggsave("plot/SF6_raw.png", width = 16, height = 8)
 
 dt_velocicalc[!is.na(test_id)] %>% 
   ggplot(aes(datetime, vel, color = paste("VC-", vc_unit))) +
@@ -20,10 +17,12 @@ dt_velocicalc[!is.na(test_id)] %>%
   scale_color_discrete("VC unit")
 
 dt_velocicalc[!is.na(test_id)] %>% 
-  ggplot(aes(datetime, vel, color = paste(loc, s_r))) +
+  ggplot(aes(datetime, vel, color = paste(loc, s_r, "\nVC-", vc_unit))) +
   geom_line() + 
   facet_wrap(vars(test_id), scales = "free") +
-  scale_color_discrete("VC unit")
+  scale_color_discrete("VC unit\nlocation")+
+  guides(color = guide_legend(nrow = 1)) +
+  theme(legend.position = "top")
 
 dt_wind_sensor[!is.na(test_id)] %>% 
   ggplot(aes(datetime, vel_mag , color = paste("Station", wind_sensor_unit))) +
